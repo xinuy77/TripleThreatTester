3788d7227c7f463f2263fdae9de2571e4b5317cf
xinuy77 <yuyamanaka@cmail.carleton.ca>
2018-03-21 20:55:22 -0400

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Password Scheme Application
+- COMP3008 Project Part 2
---
+++ app.js
@@ -0,0 +1,117 @@
+
+/* Module includes*/
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+let qstring = require('querystring');
+var url = require('url');
+let app = express();
+
+/* Constants */
+ const PORT = process.env.PORT || 3000;
+ const ROOT = __dirname + '/public/';
+
+/* Middle Ware */
+app.use(express.static(ROOT));
+
+/* Routes */
+
+/*
+  Timestamp, UserId, Client Header (browser), Result of login,
+  Format: {Log: {timeStamp: “”, userId: number, header: “”, result: “”}}
+*/
+app.post('/log', function(req, res)
+{
+  let reqData = ''
+  req.on('data', function (chunk) {
+    reqData += chunk
+  })
+  req.on('end', function()
+  {
+    /* TODO enter user json into database see format above for data */
+    let dataObj = JSON.parse(reqData);
+    console.log(dataObj);
+    console.log('timeStamp: ' +
+                 dataObj.Log.timeStamp + ', UserID: ' +
+                 dataObj.Log.userId + ', Header: ' +
+                 dataObj.Log.header + ', result: ' +
+                 dataObj.Log.result);
+
+    res.send('<h1>Post Log data</h1>');
+  })
+});
+
+/*
+  Expecting {Name: "userID"} as input to server
+  Send this format to client: {Log: {timeStamp: “”, userId: number, header: “”, result: “”}}
+*/
+app.post('/getLog', function(req, res)
+{
+  let reqData = ''
+  req.on('data', function (chunk) {
+    reqData += chunk
+  })
+  req.on('end', function()
+  {
+    /* TODO Get data from database matching client ID then send back to them */
+    let dataObj = JSON.parse(reqData);
+    //userID is the name for the user, so send back the users data
+    let userID = dataObj.Name;
+    console.log('userID is: ' + userID);
+    res.send('<h1>get Log data</h1>');
+  })
+});
+
+/* Expected format {userId: number, password: “passwd”} */
+app.post('/login', function(req, res)
+{
+  let reqData = ''
+  req.on('data', function (chunk) {
+    reqData += chunk
+  })
+  req.on('end', function()
+  {
+    //This will contain userID and password
+    let dataObj = JSON.parse(reqData);
+    let userID = dataObj.userId;
+    let password = dataObj.password;
+
+    console.log('User ID: ' + userID + ', Password is: ' + password);
+    //TODO
+    //Check Attempts to see if they exceeded the number of attempts they have
+    //Get users stored password and compare to given password
+    //If correct send back to user success, set attempts to 0
+    //If not correct send back to user failed, then set attempts += 1
+    res.send('<h1>post loggin</h1>');
+  })
+});
+
+app.post('/register', function(req, res)
+{
+  let reqData = ''
+  req.on('data', function (chunk) {
+    reqData += chunk
+  })
+  req.on('end', function()
+  {
+    //This will contain userID and password
+    let dataObj = JSON.parse(reqData);
+    let userID = dataObj.userId;
+    let password = dataObj.password;
+
+    console.log('User ID: ' + userID + ', Password is: ' + password);
+    //TODO
+    //check for duplicate ID's
+    //Add users to database
+    //Send back success or failure username already taken
+    res.send('<h1>post loggin</h1>');
+  })
+});
+
+/* Start server */
+ app.listen(PORT, start);
+
+ function start()
+ {
+   console.log('Server running on port: ' + PORT);
+ }
